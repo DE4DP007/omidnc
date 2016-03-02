72207616e38318ff407a2b6984ee4129428e9854
De4Dp007 <yangchenfu2@gmail.com>
2016-03-02 17:29:15 +0300

    first cutting added

--- bitrix/templates/furniture_pale-blue/footer.php
+++ bitrix/templates/furniture_pale-blue/footer.php
@@ -1,41 +1,51 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-IncludeTemplateLangFile(__FILE__);
-?>
-			</div>
-		</div>
-		<div id="space-for-footer"></div>
-	</div>
-	
-	<div id="footer">
-	
-		<div id="copyright">
-<?
-$APPLICATION->IncludeFile(
-	SITE_DIR."include/copyright.php",
-	Array(),
-	Array("MODE"=>"html")
-);
-?>
-		</div>
-		<div class="footer-links">	
-<?
-$APPLICATION->IncludeComponent("bitrix:menu", "bottom", array(
-	"ROOT_MENU_TYPE" => "bottom",
-	"MENU_CACHE_TYPE" => "N",
-	"MENU_CACHE_TIME" => "36000000",
-	"MENU_CACHE_USE_GROUPS" => "Y",
-	"MENU_CACHE_GET_VARS" => array(
-	),
-	"MAX_LEVEL" => "1",
-	"CHILD_MENU_TYPE" => "left",
-	"USE_EXT" => "N",
-	"ALLOW_MULTI_SELECT" => "N"
-	),
-	false
-);
-?>
-		</div>
-		<div id="footer-design"><?=GetMessage("FOOTER_DISIGN")?></div>
-	</div>
+IncludeTemplateLangFile(__FILE__);?>
+
+
+    <hr/>
+
+    <!-- Call to Action Section -->
+    <div class="well">
+        <div class="row">
+            <div class="col-md-8">
+                <p>
+                    По всем интересующим Вас вопросам Вы можете связаться с нами с помощью контактной формы или посетив один из наших открытых семинаров.
+                    Мы всегда рады сотрудничеству.
+                </p>
+            </div>
+            <div class="col-md-4">
+                <a class="btn btn-lg btn-default btn-block" href="#">Связаться с нами</a>
+            </div>
+        </div>
+    </div>
+
+    <hr/>
+
+    <!-- Footer -->
+    <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p>&copy; 2016 Отдел математики и информатики <br/>Дагестанского научного центра <br/>Российской академии наук</p>
+            </div>
+        </div>
+    </footer>
+
+</div>
+<!-- /.container -->
+
+<!-- jQuery -->
+<script type="text/javascript" src="<?=SITE_TEMPLATE_PATH?>/js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script type="text/javascript" src="<?=SITE_TEMPLATE_PATH?>/js/bootstrap.min.js"></script>
+
+<!-- Script to Activate the Carousel -->
+<script type="text/javascript">
+    $('.carousel').carousel({
+        interval: 12000 //changes the speed
+    })
+</script>
+
 </body>
+
 </html>
--- bitrix/templates/furniture_pale-blue/header.php
+++ bitrix/templates/furniture_pale-blue/header.php
@@ -1,157 +1,146 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<?
-IncludeTemplateLangFile(__FILE__);
-?> 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+<?IncludeTemplateLangFile(__FILE__);?>
+
+<!DOCTYPE html>
+<html lang="ru">
+
 <head>
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<?$APPLICATION->ShowHead();?>
-<link href="<?=SITE_TEMPLATE_PATH?>/common.css" type="text/css" rel="stylesheet" />
-<link href="<?=SITE_TEMPLATE_PATH?>/colors.css" type="text/css" rel="stylesheet" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<!--[if lte IE 6]>
-	<style type="text/css">
-		
-		#banner-overlay { 
-			background-image: none;
-			filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?=SITE_TEMPLATE_PATH?>images/overlay.png', sizingMethod = 'crop'); 
-		}
-		
-		div.product-overlay {
-			background-image: none;
-			filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?=SITE_TEMPLATE_PATH?>images/product-overlay.png', sizingMethod = 'crop');
-		}
-		
-	</style>
-	<![endif]-->
+    <?$APPLICATION->ShowHead();?>
 
-	<title><?$APPLICATION->ShowTitle()?></title>
+    <meta name="description" content="Отдел математики и информатики Дагестанского научного центра Российской академии наук">
+    <meta name="author" content="Интеллектуальные системы">
+    <meta name="keywords" content="математика, информатика, прикладная математика, научно-исследовательская работа, Дагестанский научный центр, РАН, ДНЦ РАН">
+    <link href="<?=SITE_TEMPLATE_PATH?>/favicon.ico" rel="icon shortcut" type="image/x-icon" />
+
+
+    <!-- <title>Отдел математики и информатики ДНЦ РАН</title> -->
+    <title><?$APPLICATION->ShowTitle()?></title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="<?=SITE_TEMPLATE_PATH?>/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="<?=SITE_TEMPLATE_PATH?>/css/modern-business.css" rel="stylesheet">
+
+    <link href="<?=SITE_TEMPLATE_PATH?>/css/omistyles.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="<?=SITE_TEMPLATE_PATH?>/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+
+    <!-- From Bitrix. Do we need it? -->
+    <!--[if lte IE 6]>
+    <style type="text/css">
+        #banner-overlay {
+            background-image: none;
+            filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?=SITE_TEMPLATE_PATH?>images/overlay.png', sizingMethod = 'crop');
+        }
+        div.product-overlay {
+            background-image: none;
+            filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?=SITE_TEMPLATE_PATH?>images/product-overlay.png', sizingMethod = 'crop');
+        }
+    </style>
+    <![endif]-->
+
 </head>
-<body>
-	<div id="page-wrapper">
-	<div id="panel"><?$APPLICATION->ShowPanel();?></div>
-		<div id="header">
-			
-			<table id="logo">
-				<tr>
-					<td><a href="<?=SITE_DIR?>" title="<?=GetMessage('CFT_MAIN')?>"><?
-$APPLICATION->IncludeFile(
-	SITE_DIR."include/company_name.php",
-	Array(),
-	Array("MODE"=>"html")
-);
-?></a></td>
-				</tr>
-			</table>
-			
-			<div id="top-menu">
-				<div id="top-menu-inner">
-<?$APPLICATION->IncludeComponent("bitrix:menu", "horizontal_multilevel", array(
-	"ROOT_MENU_TYPE" => "top",
-	"MAX_LEVEL" => "2",
-	"CHILD_MENU_TYPE" => "left",
-	"USE_EXT" => "Y",
-	"MENU_CACHE_TYPE" => "A",
-	"MENU_CACHE_TIME" => "36000000",
-	"MENU_CACHE_USE_GROUPS" => "Y",
-	"MENU_CACHE_GET_VARS" => ""
-	),
-	false,
-	array(
-	"ACTIVE_COMPONENT" => "Y"
-	)
-);?>
-				</div>
-			</div>
-			
-			<div id="top-icons">
-				<a href="<?=SITE_DIR?>" class="home-icon" title="<?=GetMessage('CFT_MAIN')?>"></a>
-				<a href="<?=SITE_DIR?>search/" class="search-icon" title="<?=GetMessage('CFT_SEARCH')?>"></a>
-				<a href="<?=SITE_DIR?>contacts/" class="feedback-icon" title="<?=GetMessage('CFT_FEEDBACK')?>"></a>
-			</div>
-		
-		</div>
-		
-		<div id="banner">		
-			<table id="banner-layout" cellspacing="0">
-				<tr>
-					<td id="banner-image"><div><img src="<?=SITE_TEMPLATE_PATH?>/images/head.jpg" /></div></td>
-					<td id="banner-slogan">
-<?
-$APPLICATION->IncludeFile(
-	SITE_DIR."include/motto.php",
-	Array(),
-	Array("MODE"=>"html")
-);
-?>
-					</td>
-				</tr>
-			</table>
-			<div id="banner-overlay"></div>	
-		</div>
-		
-		<div id="content">
-		
-			<div id="sidebar">
-<?$APPLICATION->IncludeComponent("bitrix:menu", "left", array(
-	"ROOT_MENU_TYPE" => "left",
-	"MENU_CACHE_TYPE" => "A",
-	"MENU_CACHE_TIME" => "36000000",
-	"MENU_CACHE_USE_GROUPS" => "Y",
-	"MENU_CACHE_GET_VARS" => array(
-	),
-	"MAX_LEVEL" => "1",
-	"CHILD_MENU_TYPE" => "left",
-	"USE_EXT" => "Y",
-	"ALLOW_MULTI_SELECT" => "N"
-	),
-	false,
-	array(
-		"ACTIVE_COMPONENT" => "Y"
-	)
-);?>
-				<div class="content-block">
-					<div class="content-block-inner">
-						<h3><?=GetMessage('CFT_NEWS')?></h3>
-<?
-$APPLICATION->IncludeFile(
-	SITE_DIR."include/news.php",
-	Array(),
-	Array("MODE"=>"html")
-);
-?>
-					</div>
-				</div>
-				
-				<div class="content-block">
-					<div class="content-block-inner">
-						
-<?
-$APPLICATION->IncludeComponent("bitrix:search.form", "flat", Array(
-	"PAGE" => "#SITE_DIR#search/",
-),
-	false
-);
-?>
-					</div>
-				</div>
 
-				<div class="information-block">
-					<div class="top"></div>
-					<div class="information-block-inner">
-						<h3><?=GetMessage('CFT_FEATURED')?></h3>
-<?
-$APPLICATION->IncludeFile(
-	SITE_DIR."include/random.php",
-	Array(),
-	Array("MODE"=>"html")
-);
-?>						
-					</div>
-					<div class="bottom"></div>
-				</div>
-			</div>
-		
-			<div id="workarea">
-				<h1 id="pagetitle"><?$APPLICATION->ShowTitle(false);?></h1>
+<body>
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <!-- Bitrix Panel -->
+    <div id="panel"><?$APPLICATION->ShowPanel();?></div>
+
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header text-center" >
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Показать меню</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" id="omilogo" href="index.html">
+                Отдел Математики и Информатики
+                <br><small>Дагестанского научного центра РАН</small>
+            </a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <?$APPLICATION->IncludeComponent("bitrix:menu", "omidnc", array(
+                "ROOT_MENU_TYPE" => "top",
+                "MAX_LEVEL" => "2",
+                "CHILD_MENU_TYPE" => "left",
+                "USE_EXT" => "Y",
+                "MENU_CACHE_TYPE" => "A",
+                "MENU_CACHE_TIME" => "36000000",
+                "MENU_CACHE_USE_GROUPS" => "Y",
+                "MENU_CACHE_GET_VARS" => ""
+            ),
+                false,
+                array(
+                    "ACTIVE_COMPONENT" => "Y"
+                )
+            );?>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+
+<!-- ========== MAKE IT ON MAIN PAGE ONLY! =========== -->
+<!-- Header Carousel -->
+<header id="myCarousel" class="carousel slide">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+        <div class="item active">
+            <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');"></div>
+            <div class="carousel-caption">
+                <h2>Отдел Математики и Информатики</h2>
+            </div>
+        </div>
+        <div class="item">
+            <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
+            <div class="carousel-caption">
+                <h2>Заседание научного семинара</h2>
+            </div>
+        </div>
+        <div class="item">
+            <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
+            <div class="carousel-caption">
+                <h2>Ученые Отдела за работой</h2>
+            </div>
+        </div>
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="icon-prev"></span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="icon-next"></span>
+    </a>
+</header>
+
+
+
+
+<!-- Page Content -->
+<div class="container">
