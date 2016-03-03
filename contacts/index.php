b7fb77dca995cbe563e13252d869a8776c9a3d3a
De4Dp007 <yangchenfu2@gmail.com>
2016-03-03 17:49:43 +0300

    scients small list

--- contacts/index.php
+++ contacts/index.php
@@ -52,6 +52,7 @@ $APPLICATION->SetTitle("Контакты");
         <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>
         -->
     </div>
+
     <!-- Contact Details Column -->
     <div class="col-md-4">
         <h3>
@@ -111,6 +112,8 @@ $APPLICATION->SetTitle("Контакты");
         <?//============= MAKE A COMPONENT! ==============?>
 
     </div>
+
+
 </div>
 <!-- /.row -->
 
--- index.php
+++ index.php
@@ -151,6 +151,91 @@ $APPLICATION->SetTitle("Отдел Математики и Информатик�
                 );?>
             </h2>
         </div>
+
+        <?$APPLICATION->IncludeComponent(
+	"bitrix:news.list", 
+	"scientistlist", 
+	array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"COMPONENT_TEMPLATE" => "scientistlist",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "5",
+		"IBLOCK_TYPE" => "news",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "20",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array(
+			0 => "",
+			1 => "RANK",
+			2 => "NAME",
+			3 => "PATRONIM",
+			4 => "DEGREE",
+			5 => "SURNAME",
+			6 => "",
+		),
+		"SET_BROWSER_TITLE" => "Y",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "Y",
+		"SET_META_KEYWORDS" => "Y",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "Y",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC"
+	),
+	false
+);?>
+
+    </div>
+
+
+    <!-- Team Members -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h2 class="page-header">
+                <?$APPLICATION->IncludeFile(
+                    SITE_DIR."include/mainhead3.php",
+                    Array(),
+                    Array("MODE"=>"text")
+                );?>
+            </h2>
+        </div>
         <div class="col-md-4 text-center">
             <div class="thumbnail">
                 <img class="img-responsive" src="http://placehold.it/750x450" alt="">
